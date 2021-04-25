806742f9b972d782fe0a445d0327fae97a5b36c4
DJmong <49215689+DJmong@users.noreply.github.com>
2021-04-25 19:24:30 +0900

    Add Background file

--- a.py
+++ a.py
@@ -1,20 +1,29 @@
 import pygame
 
 Color = (255, 255, 255)
-width = 800
-height = 600
+width = 740
+height = 345
+background_width = width
+
+def back(background, x, y):
+    global gamepad
+    gamepad.blit(background, (x,y))
 
 def airplane(x, y):
     global gamepad, aircraft
     gamepad.blit(aircraft, (x, y))
 
 def runGame():
-    global gamepad, clock
+    global gamepad, clock, background1, background2
 
     x = width * 0.05
     y = height * 0.8
     y_change = 0
 
+    background1_x = 0
+    background2_x = background_width
+    speed = 2
+    
     crashed = False
     while not crashed:
         for event in pygame.event.get():
@@ -32,8 +41,22 @@ def runGame():
                     y_change = 0
         
         y += y_change
-
         gamepad.fill(Color)
+        
+        background1_x -= speed
+        background2_x -= speed
+        
+        if background1_x <= -background_width:
+            background1_x = background_width
+        
+        
+        if background2_x <= -background_width:
+           background2_x = background_width
+        
+        
+        back(background1, background1_x, 0)
+        back(background2, background2_x, 0)
+        
         airplane(x, y)
         pygame.display.update()
         clock.tick(60)
@@ -42,13 +65,15 @@ def runGame():
     quit()
 
 def initGame():
-    global gamepad, aircraft, clock
+    global gamepad, aircraft, clock, background1, background2
 
     pygame.init()
     gamepad = pygame.display.set_mode((width, height))
     pygame.display.set_caption("Test")
     aircraft = pygame.image.load('plane.png')
-
+    background1 = pygame.image.load('background.png')
+    background2 = background1.copy()
+    
     clock = pygame.time.Clock()
     runGame()
 
