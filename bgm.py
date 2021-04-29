114bd749b7da621e4849586046f48b5bf6ce0675
Lee <ldj9604@gmail.com>
2021-04-29 23:54:26 +0900

    Make Bgm Class Next > Add Sound Effect Function

--- bgm.py
+++ bgm.py
@@ -1,13 +1,30 @@
 import pygame
 
-def playmusic(soundfile):
-    pygame.mixer.init()
-    pygame.mixer.music.load(soundfile)
-    pygame.mixer.music.play(-1)
-    pygame.mixer.music.set_volume(0.3)
+class Bgm():
+    def __init__(self):
+        pygame.mixer.init()
+        self.setVolume(0.5)
+        self.file = ''
 
-def stopmusic():
-    pygame.mixer.music.stop()
+    def __init__(self, file):
+        pygame.mixer.init()
+        self.setVolume(0.5)
+        self.setMusic(file)
+
+    def setMusic(self, file):
+        self.file = file
+
+    def playMusic(self):
+        pygame.mixer.music.load(self.file)
+        pygame.mixer.music.play(-1)
+        pygame.mixer.music.set_volume(self.volume)
+
+    def setVolume(self, volume):
+        self.volume = volume
+
+    def stopmusic(self):
+        pygame.mixer.music.stop()
+
 
 def test():
     while(1):
@@ -15,5 +32,6 @@ def test():
 
 if __name__ == '__main__':
     pygame.init()
-    playmusic('music.mp3')
+    b = Bgm('music.mp3')
+    b.playMusic()
     test()
