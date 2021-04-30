4fd4aff18593fa38a1d777f1786221cb0110dd69
Lee <ldj9604@gmail.com>
2021-04-30 23:16:03 +0900

    Add Sound Effect

--- bgm.py
+++ bgm.py
@@ -14,7 +14,7 @@ class Bgm():
     def setMusic(self, file):
         self.file = file
 
-    def playMusic(self):
+    def play(self):
         pygame.mixer.music.load(self.file)
         pygame.mixer.music.play(-1)
         pygame.mixer.music.set_volume(self.volume)
@@ -25,6 +25,20 @@ class Bgm():
     def stopmusic(self):
         pygame.mixer.music.stop()
 
+class Sfx():
+    def __init__(self):
+        pygame.mixer.init()
+        self.file = ''
+
+    def setMusic(self, file):
+        self.file = file
+
+    def play(self, file):
+        Sound = pygame.mixer.Sound('sfx.mp3')
+        pygame.mixer.Sound.play(Sound)
+
+    def stopmusic(self):
+        pygame.mixer.Sound.stop()
 
 def test():
     while(1):
@@ -33,5 +47,7 @@ def test():
 if __name__ == '__main__':
     pygame.init()
     b = Bgm('music.mp3')
-    b.playMusic()
+    c = Sfx()
+    b.play()
+    c.play('sfx.mp3')
     test()
