0c846e3d4d689f7b36bd26c53414bbfeae83e707
DJMong <ldj9604@gmail.com>
2021-05-12 13:37:41 +0900

    syntax error fix.

--- main.py
+++ main.py
@@ -175,7 +175,7 @@ def runGame():
 
                 if bxy[0] >= width:
                     try:
-                        bulletxy.remove(bxy)
+                        bullet_xy.remove(bxy)
                     except:
                         pass
         
