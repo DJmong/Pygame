5d0daab295fa64fb47c92a5e995872140013f802
Lee <ldj9604@gmail.com>
2021-05-18 23:29:56 +0900

    Add Super Bat

--- source/enemy.py
+++ source/enemy.py
@@ -12,6 +12,9 @@ class Bat(ch.Enemy):
         self.setLocation(res.width, random.randrange(0, res.height - self.wh[1]))
         self.setAccel(-6, 0)
         
-    def __del__(self):
-        super().__del__()
-    
+class SuperBat(ch.Enemy):
+     def __init__(self):
+        super().__init__(80, 'graphic/bat.png')
+        self.setSize(300, 240)
+        self.setLocation(res.width, random.randrange(0, res.height - self.wh[1]))
+        self.setAccel(-1, 0)
